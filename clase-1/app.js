a8c6dc33f96061fc29888616558d106a7305a2d1
diegojavierconchillowagner <Diegoo.javier.wagner@gmail.com>
2021-08-08 17:35:43 -0300

    upgrade

--- clase-1/app.js
+++ clase-1/app.js
@@ -1,66 +1,67 @@
 class Usuario {
     constructor(nombre, apellido, libros, mascotas ) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.libros = libros ;
-        this.mascotas = mascotas 
+        this.nombre = nombre ||'';
+        this.apellido = apellido || '';
+        this.libros = libros || [];
+        this.mascotas = mascotas || [];
     }
 
     getFullName(){
-        console.log(`hola soy ${this.nombre} ${this.apellido}`)
+        return `hola soy ${this.nombre} ${this.apellido}`
     }
     addMascota(mascota){
         this.mascotas.push(mascota)
     }
     getMascotas(){
-        console.log(this.mascotas.length)
+        return this.mascotas.length
     }
     addBook(nombre , autor){
         this.libros.push({nombre: nombre, autor: autor})
     }
     getBooks(){
-        console.log(this.libros.filter(el => console.log(el.nombre)))
+        // return this.libros.filter(el => console.log(el.nombre))
+        return this.libros.map(el => el.nombre)
     }
 
 }
 
 let diego = new Usuario('Diego','Wagner',[{nombre: 'El señor de las moscas',autor: 'William Golding'}, {nombre: 'Fundacion', autor: 'Isaac Asimov'}],['Emma','Rocky'])
-diego.getFullName();
-diego.getBooks();
-diego.getMascotas()
-diego.addBook( 'book' , 'autor' )
-diego.addMascota( 'mascotaPepe' )
+console.log(diego.getFullName());
+console.log(diego.getBooks())
+console.log(diego.getMascotas())
+diego.addBook( 'book' , 'autor' );
+diego.addMascota( 'mascotaPepe' );
 console.log(diego)
 
 // function Usuario(nombre, apellido, libros, mascotas ) {
 
-//         this.nombre = nombre;
-//         this.apellido = apellido;
-//         this.libros = libros ;
-//         this.mascotas = mascotas 
+//             this.nombre = nombre ||'';
+//             this.apellido = apellido || '';
+//             this.libros = libros || [];
+//             this.mascotas = mascotas || [];
 //     }
 
 //     Usuario.prototype.getFullName = function(){
-//         return`hola soy ${this.nombre} ${this.apellido}`
+//         return `hola soy ${this.nombre} ${this.apellido}`
 //     }
 //     Usuario.prototype.addMascota = function(mascota){
 //         this.mascotas.push(mascota)
 //     }
 //     Usuario.prototype.getMascotas = function(){
-//         console.log(this.mascotas.length)
+//         return this.mascotas.length
 //     }
 //     Usuario.prototype.addBook = function(nombre , autor){
 //         this.libros.push({nombre: nombre, autor: autor})
 //     }
 //     Usuario.prototype.getBooks = function(){
-//         console.log(this.libros.filter(el => console.log(el.nombre)))
+//         return this.libros.map(el => el.nombre)
 //     }
 
 
 // let diego = new Usuario('Diego','Wagner',[{nombre: 'El señor de las moscas',autor: 'William Golding'}, {nombre: 'Fundacion', autor: 'Isaac Asimov'}],['Emma','Rocky'])
-// diego.getFullName();
-// diego.getBooks();
-// diego.getMascotas()
-// diego.addBook( 'book' , 'autor' )
-// diego.addMascota( 'mascotaPepe' )
+// console.log(diego.getFullName());
+// console.log(diego.getBooks())
+// console.log(diego.getMascotas())
+// diego.addBook( 'book' , 'autor' );
+// diego.addMascota( 'mascotaPepe' );
 // console.log(diego)
